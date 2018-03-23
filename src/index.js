340b77ba617f863feef06e7c1888f206f4ef452f
Alersandr Borkov <sbor767@yandex.ru>
2018-03-24 01:11:53 +0300

    Chap.02 Add yarn command (w webpack).

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-require('./index2')
-console.log('hello from index.js')
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+ReactDOM.render(
+    React.createElement('h1', null, 'Hello from react!'),
+    document.getElementById('root')
+)
