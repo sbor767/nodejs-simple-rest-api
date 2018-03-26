c12cd97dbddb54ece42db83b2e13aed88e624ae3
Alersandr Borkov <sbor767@yandex.ru>
2018-03-26 23:40:18 +0300

    Chap.02 Refactored to comply JS standards.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom'
 import App from './App'
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+  <App/>,
+  document.getElementById('root')
 )
 
 if (module.hot) {
@@ -12,7 +12,7 @@ if (module.hot) {
     // @TODO Why need NextApp ??
     const NextApp = require('./App').default
     ReactDOM.render(
-      <App />,
+      <App/>,
       document.getElementById('root')
     )
   })
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,40 @@
 let webpack = require('webpack')
 
 module.exports = {
-    entry: [
-      'react-hot-loader/patch',
-      'webpack-dev-server/client?http://localhost:8080',
-      'webpack/hot/only-dev-server',
-      __dirname + '/src/index.js'
-    ],
-    output: {
-        path: __dirname + '/public',
-        filename: 'bundle.js',
-        publicPath: '/'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015', 'react'],
-                    plugins: [
-                      'react-hot-loader/babel',
-                      'transform-class-properties'
-                    ]
-                }
-            }
-        ]
-    },
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    __dirname + '/src/index.js'
+  ],
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: [
+            'react-hot-loader/babel',
+            'transform-class-properties'
+          ]
+        }
+      }
+    ]
+  },
   plugins: [
     new webpack.HotModuleReplacementPlugin()
   ],
-    devServer: {
-      contentBase: './public',
-      historyApiFallback: true,
-      inline: true,
-      hot: true
-    }
+  devServer: {
+    contentBase: './public',
+    historyApiFallback: true,
+    inline: true,
+    hot: true
+  }
 }
