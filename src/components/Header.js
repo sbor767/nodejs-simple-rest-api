00e8fb819765555a0e60df4932c134faa802746a
Alersandr Borkov <sbor767@yandex.ru>
2018-03-29 01:00:30 +0300

    Chap.03 Ready.

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => (
+  <div id='Header'>
+    <img src='/assets/icon.png' alt='logo' />
+    <h1>Chatastrophe</h1>
+  </div>
+)
+
+export default Header
--- src/components/LoginContainer.js
+++ src/components/LoginContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Header from './Header'
 
 class LoginContainer extends Component {
   state = { email: '', password: '' }
@@ -12,10 +13,7 @@ class LoginContainer extends Component {
 
   render = () => (
     <div id='LoginContainer' className='inner-container'>
-      <div id='Header'>
-        <img src='/assets/icon.png' alt='logo' />
-        <h1>Chatastrophe</h1>
-      </div>
+      <Header />
       <form onSubmit={this.handleSubmit}>
         <p>Sign in or sign up by entering your email and password.</p>
         <input
