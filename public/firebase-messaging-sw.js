3828088ffbb2e28b97ed00f06e703fa4a4d159e9
Alersandr Borkov <sbor767@yandex.ru>
2018-04-02 00:40:36 +0300

    Chap.07 Ready.

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,23 @@
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js')
+
+/*
+self.addEventListener('install', function() {
+  console.log('Install!')
+})
+
+self.addEventListener('activate', function() {
+  console.log('Activate!')
+})
+
+self.addEventListener('fetch', function(event) {
+  // console.log('Fetch!', event.request)
+})
+*/
+
+// @TODO Exclude Id from git!
+firebase.initializeApp({
+  'messagingSenderId': '645376026703'
+})
+
+console.log(firebase.messaging())
