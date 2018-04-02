38a6626ec5e16eecbc0b548a9ea1ecdbb008a005
Alersandr Borkov <sbor767@yandex.ru>
2018-04-02 19:35:47 +0300

    Chap.08 Functions/ - Sending to the tokens.

--- functions/index.js
+++ functions/index.js
@@ -24,4 +24,33 @@ exports.sendNotifications = functions.database
         click_action: `https://${functions.config().firebase.authDomain}`
       }
     }
+    return admin
+      .database()
+      .ref('fcmTokens')
+      .once('value')
+      .then(allTokens => {
+        if (allTokens.val()) {
+          const tokens = []
+          for (let fcmTokenKey in allTokens.val()) {
+            const fcmToken = allTokens.val()[fcmTokenKey]
+            if (fcmToken.user_id !== snapshot.val().user_id) tokens.push(fcmToken.token)
+          }
+          if (tokens.length > 0) return admin
+            .messaging()
+            .sendToDevice(tokens, payload)
+            .then(response => {
+              const tokensToRemove = []
+              response.results.forEach((result, index) => {
+                const error = result.error
+                if (error) {
+                  console.error('Failure sending notification to', tokens[index], error)
+                  if (error.code === 'messaging/invalid-registration-token' || error.code === 'messaging/registration-token-not-registered') {
+                    tokensToRemove.push(allTokens.ref.child(tokens[index]).remove())
+                  }
+                }
+              })
+              return Promise.all(tokensToRemove)
+            })
+        }
+      })
   })
