18acfc6a34e28ba41e2faac31b580f61921c34ed
Alersandr Borkov <sbor767@yandex.ru>
2018-04-02 22:44:49 +0300

    Chap.08 Functions/ - Sending to the tokens //Fix errs.

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,4 @@
-const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
+const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 admin.initializeApp(functions.config().firebase)
 
@@ -35,22 +27,24 @@ exports.sendNotifications = functions.database
             const fcmToken = allTokens.val()[fcmTokenKey]
             if (fcmToken.user_id !== snapshot.val().user_id) tokens.push(fcmToken.token)
           }
-          if (tokens.length > 0) return admin
-            .messaging()
-            .sendToDevice(tokens, payload)
-            .then(response => {
-              const tokensToRemove = []
-              response.results.forEach((result, index) => {
-                const error = result.error
-                if (error) {
-                  console.error('Failure sending notification to', tokens[index], error)
-                  if (error.code === 'messaging/invalid-registration-token' || error.code === 'messaging/registration-token-not-registered') {
-                    tokensToRemove.push(allTokens.ref.child(tokens[index]).remove())
+          if (tokens.length > 0) {
+            return admin
+              .messaging()
+              .sendToDevice(tokens, payload)
+              .then(response => {
+                const tokensToRemove = []
+                response.results.forEach((result, index) => {
+                  const error = result.error
+                  if (error) {
+                    console.error('Failure sending notification to', tokens[index], error)
+                    if (error.code === 'messaging/invalid-registration-token' || error.code === 'messaging/registration-token-not-registered') {
+                      tokensToRemove.push(allTokens.ref.child(tokens[index]).remove())
+                    }
                   }
-                }
+                })
+                return Promise.all(tokensToRemove)
               })
-              return Promise.all(tokensToRemove)
-            })
+          }
         }
       })
   })
