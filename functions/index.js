4d73d3def0d3018aa9cf7f7215fcac487c70307e
Alersandr Borkov <sbor767@yandex.ru>
2018-04-02 19:04:20 +0300

    Chap.08 Functions/ - Writing our Cloud function.

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,22 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const admin = require('firebase-admin')
+admin.initializeApp(functions.config().firebase)
+
+exports.sendNotifications = functions.database
+  .ref('messages/{messageId}')
+  .onWrite(event => {
+    const snapshot = event.data
+    if (snapshot.previous.val()) return
+
+    const payload = {
+      notification: {
+        title: `${snapshot.val().author}`,
+        body: `${snapshot.val().msg}`,
+        icon: 'assets/icon.png',
+        click_action: `https://${functions.config().firebase.authDomain}`
+      }
+    }
+  })
