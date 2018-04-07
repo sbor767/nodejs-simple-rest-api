d7c690d52fe525eb732871ed18e0abe69c6ddc88
Alersandr Borkov <sbor767@yandex.ru>
2018-04-08 00:36:34 +0300

    Change Firebase CDN from 3.9.0 to 4.12.0

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js')
+importScripts('https://www.gstatic.com/firebasejs/4.12.0/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/4.12.0/firebase-messaging.js')
 
 /*
 self.addEventListener('install', function() {
